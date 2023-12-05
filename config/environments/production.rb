5c6f17574276eea0d92f6edcef333831e8c90177
Ashley <asyan77@gmail.com>
2023-12-05 12:17:10 -0800

    test for deploying5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_storage.service = :amazon_prod
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
