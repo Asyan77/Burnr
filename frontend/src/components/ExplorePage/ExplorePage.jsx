c6bfcac8634c1935dd8400004248365c84abd6b2
Ashley <asyan77@gmail.com>
2024-03-06 18:30:17 -0800

    edit photo size on Explore page & made photos a random grid of 20 pics

--- frontend/src/components/ExplorePage/ExplorePage.jsx
+++ frontend/src/components/ExplorePage/ExplorePage.jsx
@@ -7,7 +7,21 @@ import { Link } from "react-router-dom";
 const ExplorePage =() => {
   const dispatch = useDispatch();
   const photos = useSelector(allPhotos)
-                                 
+  let randomPhotos = []
+
+  function getRandomPhotos () {
+    let start = 0;
+    let end = photos.length-1
+    while (randomPhotos.length < 20 ) {
+      let randomPicture = Math.floor(Math.random() * (end - start) + start)
+      if(!randomPhotos.includes(randomPicture)) {
+        randomPhotos.push(randomPicture)
+      }
+    }
+  }
+  getRandomPhotos()
+  
+  
   useEffect(() => {
     dispatch(getAllPhotos())
   }, [dispatch]);
@@ -17,11 +31,11 @@ const ExplorePage =() => {
       <div className="page"> 
         <div className="explore">Explore</div>
         <ul className="photoimage-grid">
-          {photos.map(photo  => {
+          {randomPhotos.map(photo  => {
             return (
-              <div key={photo.id}>
-                <Link to={`/photos/${photo.userId}/${photo.id}`}>
-                  <img key={photo.id} src={photo.photoUrl} alt="photos" className="user-photoimage" />
+              <div key={photos[photo].id}>
+                <Link className="user-photoimage" to={`/photos/${photos[photo].userId}/${photos[photo].id}`}>
+                  <img key={photo.id} src={photos[photo].photoUrl} alt="photos" className="user-photoimage" />
                 </Link>
               </div>
             );
